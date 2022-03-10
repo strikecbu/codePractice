8abccff7b0a76b046876886ed7a41812a71d650e
andychen <strikecbu@gmail.com>
2022-03-10 23:36:34 +0800

    practice with template driven form

--- angular/forms-td-start/src/app/app.component.ts
+++ angular/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') userForm: NgForm;
+  questionAnswer: string;
+  isSubmit: boolean;
+  userFormData: {
+    username: string,
+    email: string,
+    secret: string,
+    answer: string
+  };
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.userForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+        email: 'email@cdddsddo.com '
+      },
+      secret: 'teacher',
+      answer: 'hoho'
+    });
+
+  }
+
+  onSubmit(form: NgForm) {
+    this.isSubmit = true;
+    this.userFormData = {
+      username: form.form.value.userData.username,
+      email: form.form.value.userData.email,
+      secret: form.form.value.secret,
+      answer: form.form.value.answer
+    };
+
+    console.log(form);
+    console.log(form.form.valid);
+    this.onReset();
+
+  }
+
+  onReset() {
+    this.userForm.reset();
   }
 }
