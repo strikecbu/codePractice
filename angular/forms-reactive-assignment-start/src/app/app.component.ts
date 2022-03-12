0a8b819ec1bf1b9d6b3c7115bc88bbe2b156cb38
andychen <strikecbu@gmail.com>
2022-03-12 20:24:32 +0800

    Finish reactive form assignment

--- angular/forms-reactive-assignment-start/src/app/app.component.ts
+++ angular/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
+
+// import { FormGroup } from '@angular/forms'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  userForm: FormGroup;
+  forbiddenProjectNames = ["Test"];
+  isSubmit = false;
+  userData: {
+    project: {
+      name: string;
+      status: string;
+    };
+    email: string;
+  };
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      project: new FormGroup({
+        name: new FormControl(
+          null,
+          [Validators.required],
+          this.checkNameValid.bind(this)
+        ),
+        status: new FormControl(null, Validators.required),
+      }),
+      email: new FormControl(null, [Validators.email, Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    this.isSubmit = true;
+    console.log(this.userForm);
+  }
+
+  checkNameValid(control: FormControl): Observable<any> | Promise<any> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        if (this.forbiddenProjectNames.indexOf(control.value) != -1) {
+          resolve({ forbiddenNames: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+  }
 }
