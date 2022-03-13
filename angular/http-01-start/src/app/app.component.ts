ee82359b709d5fcfc0218dd14ebf71d986b319e8
andychen <strikecbu@gmail.com>
2022-03-13 23:48:54 +0800

    http implement to 260 lecture

--- angular/http-01-start/src/app/app.component.ts
+++ angular/http-01-start/src/app/app.component.ts
@@ -1,25 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Post } from "./post.model";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post<{ name: string }>(
+        "https://angulor-http-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+        this.onFetchPosts();
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.http
+      .get<{ [key: string]: Post }>(
+        "https://angulor-http-default-rtdb.firebaseio.com/posts.json"
+      )
+      .pipe(
+        map((responseData) => {
+          const posts: Post[] = [];
+          for (let key in responseData) {
+            if (responseData.hasOwnProperty(key))
+              posts.push({ ...responseData[key], id: key });
+          }
+          return posts;
+        })
+      )
+      .subscribe((posts) => {
+        this.loadedPosts = posts;
+      });
   }
 
   onClearPosts() {
