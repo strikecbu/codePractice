0419ef75bc6ee30ffcce7813ef62ff3786023798
andychen <strikecbu@gmail.com>
2022-03-17 18:19:27 +0800

    practice checkbox and radio form for reactive-form

--- angular/forms-reactive-assignment-start/src/app/app.component.ts
+++ angular/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { OnInit } from "@angular/core";
-import { Component } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { Observable } from "rxjs";
+import { Component, OnInit} from "@angular/core";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
-// import { FormGroup } from '@angular/forms'
 
 @Component({
   selector: "app-root",
@@ -32,13 +30,44 @@ export class AppComponent implements OnInit {
         ),
         status: new FormControl(null, Validators.required),
       }),
-      email: new FormControl(null, [Validators.email, Validators.required]),
+      terms: new FormControl(null, Validators.requiredTrue),
+      choice: new FormControl(null, [Validators.required]),
+      sports: new FormArray(
+        [new FormControl(null), new FormControl(null)],
+        [Validators.required]
+      ),
+      email: new FormControl(null, [Validators.email]),
     });
+
+    window["userForm"] = this.userForm;
   }
+
+  sports: { id: number; name: string }[] = [
+    {
+      id: 0,
+      name: "baseball",
+    },
+    { id: 1, name: "tennis" },
+  ];
 
   onSubmit() {
     this.isSubmit = true;
     console.log(this.userForm);
+    this.matchSportValues();
+
+  }
+
+  matchSportValues(): string[] {
+    const values: string[] = [];
+    for (let i = 0; i < this.userForm.value.sports.length; i++) {
+      if (!this.userForm.value.sports[i]) continue;
+      const sport = this.sports.find((item) => item.id === i);
+      if (sport) {
+        values.push(sport.name);
+      }
+    }
+    console.log("Your choose sports: " + values);
+    return values;
   }
 
   checkNameValid(control: FormControl): Observable<any> | Promise<any> {
