8e414262b4424933f9af6f98aa8908976309e3a0
andychen <strikecbu@gmail.com>
2022-03-30 16:41:41 +0800

    Refactor use service to excute side effect logic

--- reactive-angular/01.start/src/app/home/home.component.ts
+++ reactive-angular/01.start/src/app/home/home.component.ts
@@ -1,60 +1,63 @@
-import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
-
+import { Component, OnInit } from '@angular/core';
+import { Course, sortCoursesBySeqNo } from '../model/course';
+import { interval, noop, Observable, of, throwError, timer } from 'rxjs';
+import {
+  catchError,
+  delay,
+  delayWhen,
+  filter,
+  finalize,
+  map,
+  retryWhen,
+  shareReplay,
+  tap,
+} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
+import { CourseService } from '../services/course.service';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses$: Observable<Course[]>;
 
-  beginnerCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
-
-
-  constructor(private http: HttpClient, private dialog: MatDialog) {
-
-  }
+  constructor(
+    private http: HttpClient,
+    private dialog: MatDialog,
+    private courseService: CourseService
+  ) {}
 
   ngOnInit() {
-
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
-
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
-
+    const courses$ = this.courseService.getAllCourses().pipe(
+      shareReplay(1)
+    );
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) => {
+        return courses.filter((course) => course.category === 'BEGINNER');
+      })
+    );
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) => {
+        return courses.filter((course) => course.category === 'ADVANCED');
+      })
+    );
   }
 
   editCourse(course: Course) {
-
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
+    dialogConfig.width = '400px';
 
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
   }
-
 }
-
-
-
-
---
+++ reactive-angular/01.start/src/app/services/course.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Course } from '../model/course';
+
+@Injectable({ providedIn: 'root' })
+export class CourseService {
+  constructor(private http: HttpClient) {}
+
+  getAllCourses(): Observable<Course[]> {
+    return this.http.get('/api/courses').pipe(
+      map((resp) => {
+        return resp['payload'];
+      }),
+    );
+  }
+}
