633a6a0acfcee4b8495b7a3f6de5e3e71d673323
andychen <strikecbu@gmail.com>
2022-04-06 01:06:04 +0800

    Implement local component import and slot use for card-like component

--- vue/09.cmp-adv-01-starting-setup/src/components/BadgeList.vue
+++ vue/09.cmp-adv-01-starting-setup/src/components/BadgeList.vue
@@ -1,22 +1,30 @@
 <template>
-  <section>
-    <h2>Available Badges</h2>
-    <ul>
-      <li>
-        <base-badge type="admin" caption="ADMIN"></base-badge>
-      </li>
-      <li>
-        <base-badge type="author" caption="AUTHOR"></base-badge>
-      </li>
-    </ul>
-  </section>
+  <base-card>
+    <template #header="slotProps">
+      <h2>{{ slotProps.defaultValue }}</h2>
+    </template>
+    <template #default>
+      <ul>
+        <li>
+          <base-badge type="admin" caption="ADMIN"></base-badge>
+        </li>
+        <li>
+          <base-badge type="author" caption="AUTHOR"></base-badge>
+        </li>
+      </ul>
+    </template>
+  </base-card>
 </template>
 
-<style>
-section h2 {
-  margin: 0.5rem 0;
-  color: #3a3a3a;
-}
+<script>
+import BaseCard from "./BaseCard.vue";
+export default {
+  components: { BaseCard },
+  setup() {},
+};
+</script>
+
+<style scoped>
 ul {
   list-style: none;
   margin: 0;
--- vue/09.cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ vue/09.cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,31 +1,19 @@
 <template>
-  <section>
-    <div>
+  <base-card>
+    <template #header>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
-  </section>
+    </template>
+    <template #default>
+      <p>{{ infoText }}</p>
+    </template>
+  </base-card>
 </template>
 
 <script>
+import BaseCard from "./BaseCard.vue";
 export default {
-  props: ['fullName', 'infoText', 'role'],
+  components: { BaseCard },
+  props: ["fullName", "infoText", "role"],
 };
 </script>
-
-<style>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section div {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
