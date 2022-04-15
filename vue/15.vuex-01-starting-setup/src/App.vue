aa425618b3526a59fe2accc04cdbd95884bc6c26
andychen <strikecbu@gmail.com>
2022-04-15 20:36:50 +0800

    [Vue] Add vuex lecture resources

---
+++ vue/15.vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <base-container title="Vuex">
+    <counter></counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import Counter from './components/Counter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    Counter
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
