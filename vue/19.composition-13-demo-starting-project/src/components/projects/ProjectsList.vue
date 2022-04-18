d5d36430b9925751a7588f98676a328970c4a03d
andychen <strikecbu@gmail.com>
2022-04-18 22:16:13 +0800

    [Vue] Change OptionAPI to Composition API

--- vue/19.composition-13-demo-starting-project/src/components/projects/ProjectsList.vue
+++ vue/19.composition-13-demo-starting-project/src/components/projects/ProjectsList.vue
@@ -1,9 +1,17 @@
 <template>
   <base-container v-if="user">
     <h2>{{ user.fullName }}: Projects</h2>
-    <base-search v-if="hasProjects" @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <base-search
+      v-if="hasProjects"
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
     <ul v-if="hasProjects">
-      <project-item v-for="prj in availableProjects" :key="prj.id" :title="prj.title"></project-item>
+      <project-item
+        v-for="prj in availableProjects"
+        :key="prj.id"
+        :title="prj.title"
+      ></project-item>
     </ul>
     <h3 v-else>No projects found.</h3>
   </base-container>
@@ -13,49 +21,91 @@
 </template>
 
 <script>
+import { ref, toRefs } from '@vue/reactivity';
 import ProjectItem from './ProjectItem.vue';
+import { computed, watch } from '@vue/runtime-core';
 
 export default {
   components: {
     ProjectItem,
   },
   props: ['user'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-    };
-  },
-  computed: {
-    hasProjects() {
-      return this.user.projects && this.availableProjects.length > 0;
-    },
-    availableProjects() {
-      if (this.activeSearchTerm) {
-        return this.user.projects.filter((prj) =>
-          prj.title.includes(this.activeSearchTerm)
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    const hasProjects = computed(() => {
+      return props.user.projects && availableProjects.value.length > 0;
+    });
+    const availableProjects = computed(() => {
+      if (activeSearchTerm.value) {
+        return props.user.projects.filter((prj) =>
+          prj.title.includes(activeSearchTerm.value)
         );
       }
-      return this.user.projects;
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
+
+      return props.user.projects;
+    });
+
+    function updateSearch(val) {
+      enteredSearchTerm.value = val;
+    }
+    watch(enteredSearchTerm, (val) => {
       setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
+        if (val === enteredSearchTerm.value) {
+          activeSearchTerm.value = val;
         }
       }, 300);
-    },
-    user() {
-      this.enteredSearchTerm = '';
-    },
+    });
+
+    const { user } = toRefs(props);
+    watch(user, () => {
+      enteredSearchTerm.value = '';
+    });
+
+    return {
+      hasProjects,
+      updateSearch,
+      enteredSearchTerm,
+      availableProjects
+    }
   },
+  // data() {
+  //   return {
+  //     enteredSearchTerm: '',
+  //     activeSearchTerm: '',
+  //   };
+  // },
+  // computed: {
+  //   hasProjects() {
+  //     return this.user.projects && this.availableProjects.length > 0;
+  //   },
+  //   availableProjects() {
+  //     if (this.activeSearchTerm) {
+  //       return this.user.projects.filter((prj) =>
+  //         prj.title.includes(this.activeSearchTerm)
+  //       );
+  //     }
+  //     return this.user.projects;
+  //   },
+  // },
+  // methods: {
+  //   updateSearch(val) {
+  //     this.enteredSearchTerm = val;
+  //   },
+  // },
+  // watch: {
+  //   enteredSearchTerm(val) {
+  //     setTimeout(() => {
+  //       if (val === this.enteredSearchTerm) {
+  //         this.activeSearchTerm = val;
+  //       }
+  //     }, 300);
+  //   },
+  //   user() {
+  //     this.enteredSearchTerm = '';
+  //   },
+  // },
 };
 </script>
 
