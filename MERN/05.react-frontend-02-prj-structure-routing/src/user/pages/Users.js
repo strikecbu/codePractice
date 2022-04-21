1c8f413d08fb0a7b1472eb5f460a836b355dfa35
andychen <strikecbu@gmail.com>
2022-04-21 11:53:50 +0800

    [MERN] Implement UserList and detail components

--- MERN/05.react-frontend-02-prj-structure-routing/src/user/pages/Users.js
+++ MERN/05.react-frontend-02-prj-structure-routing/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS =[
+    {
+      id: 'u1',
+      name: 'Andy Chen',
+      image: 'https://startuplatte.com/wp-content/uploads/2019/10/Andy-Puddicombe.jpg',
+      places: 3
+    }
+  ];
+  return (
+      <UsersList items={USERS} />
+  );
 };
 
 export default Users;
