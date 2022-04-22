ceb3d444ee3ffdf111d85e047909d964d198c01d
andychen <strikecbu@gmail.com>
2022-04-23 00:25:00 +0800

    [MERN] Implement auth page with signup feature

--- MERN/05.react-frontend-02-prj-structure-routing/src/user/pages/Auth.js
+++ MERN/05.react-frontend-02-prj-structure-routing/src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../shared/components/FormElements/Input';
 import {
   VALIDATOR_EMAIL,
@@ -7,9 +7,12 @@ import {
 } from '../../shared/util/validators';
 import Button from '../../shared/components/FormElements/Button';
 import useForm from '../../shared/hooks/form-hook';
+import Card from '../../shared/components/UIElements/Card';
+import './Auth.css';
 
 const Auth = () => {
-  const [formState, inputHandler] = useForm(
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
         value: '',
@@ -28,28 +31,72 @@ const Auth = () => {
     console.log(formState.inputs);
   };
 
+  const switchModeHandler = () => {
+    if (isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: '',
+            isValid: false,
+          },
+        },
+        false
+      );
+    } else {
+      const newInputs = { ...formState.inputs };
+      delete newInputs.name;
+      setFormData(
+        {
+          ...newInputs,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    }
+    setIsLoginMode((pre) => !pre);
+  };
   return (
-    <form className="place-form" onSubmit={loginHandler}>
-      <Input
-        id="email"
-        element="input"
-        type="text"
-        label="Email"
-        validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
-        errorText="Please enter a valid email."
-        onInput={inputHandler}
-      />
-      <Input
-        id="password"
-        element="input"
-        type="password"
-        label="Password"
-        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6)]}
-        errorText="Please enter a valid password. (must least 6 characters)."
-        onInput={inputHandler}
-      />
-      <Button disabled={!formState.isValid}>Login</Button>
-    </form>
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={loginHandler}>
+        {!isLoginMode && (
+          <Input
+            id="name"
+            element="input"
+            type="text"
+            label="Your Name"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a name."
+            onInput={inputHandler}
+          />
+        )}
+        <Input
+          id="email"
+          element="input"
+          type="text"
+          label="Email"
+          validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email."
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6)]}
+          errorText="Please enter a valid password. (must least 6 characters)."
+          onInput={inputHandler}
+        />
+        <Button disabled={!formState.isValid}>
+          {isLoginMode ? 'Login' : 'SIGNUP'}
+        </Button>
+      </form>
+      <Button inverse onClick={switchModeHandler}>
+        Switch to {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+      </Button>
+    </Card>
   );
 };
 
