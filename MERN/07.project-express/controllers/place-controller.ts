0f892493cc4308a30cbc6469b00951fc76125d84
andychen <strikecbu@gmail.com>
2022-04-29 16:40:09 +0800

    [MERN] Add patch and delete handler for places

--- MERN/07.project-express/controllers/place-controller.ts
+++ MERN/07.project-express/controllers/place-controller.ts
@@ -1,7 +1,7 @@
-import { Request, Response, NextFunction } from "express";
-import { v4 } from "uuid";
+import { Request, Response, NextFunction } from 'express';
+import { v4 } from 'uuid';
 
-import HttpError from "../models/http-error";
+import HttpError from '../models/http-error';
 
 type Place = {
   id: string;
@@ -15,58 +15,58 @@ type Place = {
   };
   creator: string;
 };
-const DUMMY_PLACES: Place[] = [
+let DUMMY_PLACES: Place[] = [
   {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
       lng: -73.9878584,
     },
-    creator: "u1",
+    creator: 'u1',
   },
   {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
       lng: -73.9878584,
     },
-    creator: "u2",
+    creator: 'u2',
   },
 ];
 
 const findPlaceById = (req: Request, res: Response, next: NextFunction) => {
-  const placeId = req.params["pid"];
+  const placeId = req.params['pid'];
   const place = findPlace((place) => place.id === placeId);
   if (!place) {
     return next(
-      new HttpError("Could NOT found any place from provide pid!", 404)
+      new HttpError('Could NOT found any place from provide pid!', 404)
     );
   }
   res.json({ place });
 };
 
 const findPlaceByUserId = (req: Request, res: Response, next: NextFunction) => {
-  const userId = req.params["uid"];
+  const userId = req.params['uid'];
   const place = findPlace((place) => place.creator === userId);
   if (!place) {
     return next(
-      new HttpError("Could NOT found any place from provide uid!", 404)
+      new HttpError('Could NOT found any place from provide uid!', 404)
     );
   }
   res.json({ place });
 };
 
-const createNewPlace = (req: Request, res: Response, next: NextFunction) => {
+const createNewPlace = (req: Request, res: Response) => {
   const { title, description, address, location, creator } = req.body;
   const newPlace: Place = {
     id: v4(),
@@ -80,6 +80,37 @@ const createNewPlace = (req: Request, res: Response, next: NextFunction) => {
   res.status(201);
   res.json(newPlace);
 };
+
+export function patchPlace(req: Request, res: Response, next: NextFunction) {
+  const { title, description } = req.body;
+  const pid = req.params['pid'];
+  const place = findPlace((place) => place.id === pid);
+  if (!place) {
+    return next(new HttpError('Can not found any place by provided pid', 404));
+  }
+  const newPlace: Place = {
+    ...place,
+    title: title ?? place.title,
+    description: description ?? place.description,
+  };
+  DUMMY_PLACES = [
+    ...DUMMY_PLACES.filter((place) => place.id !== pid),
+    newPlace,
+  ];
+  res.status(200);
+  res.json(newPlace);
+}
+
+export function deletePlace(req: Request, res: Response, next: NextFunction) {
+  const pid = req.params['pid'];
+  const place = findPlace((place) => place.id === pid);
+  if (!place) {
+    return next(new HttpError('Can not found any place by provided pid', 404));
+  }
+  DUMMY_PLACES = DUMMY_PLACES.filter((place) => place.id !== pid);
+  res.status(200);
+  res.json({ pid });
+}
 
 const findPlace = (predicate: (place: Place) => boolean) => {
   return DUMMY_PLACES.find(predicate);
--- MERN/07.project-express/routes/place-routes.ts
+++ MERN/07.project-express/routes/place-routes.ts
@@ -1,12 +1,15 @@
-const express = require("express");
+const express = require('express');
 
-const placesController = require("../controllers/place-controller");
+const placesController = require('../controllers/place-controller');
 const router = express.Router();
 
-router.get("/:pid", placesController.findPlaceById);
+router.get('/:pid', placesController.findPlaceById);
 
-router.get("/user/:uid", placesController.findPlaceByUserId);
+router.get('/user/:uid', placesController.findPlaceByUserId);
 
-router.post("/", placesController.createNewPlace);
+router.post('/', placesController.createNewPlace);
+
+router.patch('/:pid', placesController.patchPlace);
+router.delete('/:pid', placesController.deletePlace);
 
 module.exports = router;
