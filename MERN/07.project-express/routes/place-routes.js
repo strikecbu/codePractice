42049bc6dc229c8ff94531ce26387c0aa02e31a5
andychen <strikecbu@gmail.com>
2022-04-29 12:08:30 +0800

    [MERN] Add Place Controller and move logic into it

--- MERN/07.project-express/routes/place-routes.js
+++ MERN/07.project-express/routes/place-routes.js
@@ -1,58 +1,10 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
-
+const placesController = require("../controllers/place-controller");
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u2",
-  },
-];
+router.get("/:pid", placesController.findPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params["pid"];
-  const place = DUMMY_PLACES.find((place) => place.id === placeId);
-  if (!place) {
-    return next(
-      new HttpError("Could NOT found any place from provide pid!", 404)
-    );
-  }
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params["uid"];
-  const place = DUMMY_PLACES.find((place) => place.creator === userId);
-  if (!place) {
-    return next(
-      new HttpError("Could NOT found any place from provide uid!", 404)
-    );
-  }
-  res.json({ place });
-});
+router.get("/user/:uid", placesController.findPlaceByUserId);
 
 module.exports = router;
