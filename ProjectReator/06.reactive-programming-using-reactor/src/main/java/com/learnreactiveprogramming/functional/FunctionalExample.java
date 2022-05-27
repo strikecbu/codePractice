d9bd2cd7786577cff7a9c5cc1c153b268139ce07
andychen <strikecbu@gmail.com>
2022-05-27 11:49:46 +0800

    [ProjectReactor] Implement two different way to filter strings

--- ProjectReator/06.reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ ProjectReator/06.reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.functional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionalExample {
+    public static void main(String[] args) {
+        var names = List.of("adm",  "cathy", "john", "roger","andy", "andy");
+        List<String> filterStrings = filterNamesByLength(names, 3);
+        System.out.println(filterStrings);
+    }
+
+    private static List<String> filterNamesByLength(List<String> names, int size) {
+       return names.stream()
+               .filter(name -> name.length() > size)
+               .distinct()
+               .map(String::toUpperCase)
+               .sorted()
+               .collect(Collectors.toList());
+    }
 }
--- ProjectReator/06.reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ ProjectReator/06.reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,4 +1,22 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImperativeExample {
+    public static void main(String[] args) {
+        var names = List.of("adm",  "cathy", "john", "roger","andy", "andy");
+        List<String> filterStrings = filterNamesByLength(names, 3);
+        System.out.println(filterStrings);
+    }
+
+    private static List<String> filterNamesByLength(List<String> names, int size) {
+        List<String> strings = new ArrayList<>();
+        for (String name : names) {
+            if (name.length()> size && !strings.contains(name.toUpperCase())) {
+                strings.add(name.toUpperCase());
+            }
+        }
+        return strings;
+    }
 }
