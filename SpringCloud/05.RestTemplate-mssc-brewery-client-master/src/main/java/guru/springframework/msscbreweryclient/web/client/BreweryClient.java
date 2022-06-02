81b433ee9bbb0b773bc8367ee74468653ecd6899
andychen <strikecbu@gmail.com>
2022-06-03 01:06:30 +0800

    [ReactiveMcSrv] implement customer client service and unit test for all rest API

--- SpringCloud/05.RestTemplate-mssc-brewery-client-master/src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ SpringCloud/05.RestTemplate-mssc-brewery-client-master/src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
     private final String BEER_API_PATH = "/api/v1/beer";
+    private final String CUSTOMER_PATH_V1 = "/api/v1/customer";
     private String apiHost;
 
     private final RestTemplate restTemplate;
@@ -40,5 +42,21 @@ public class BreweryClient {
 
     public void setApiHost(String apiHost) {
         this.apiHost = apiHost;
+    }
+
+    public CustomerDto getCustomerById(UUID customerId) {
+        return restTemplate.getForObject(apiHost+ CUSTOMER_PATH_V1 + customerId.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return  restTemplate.postForLocation(apiHost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        restTemplate.put(apiHost + CUSTOMER_PATH_V1 + customerId, customerDto);
+    }
+
+    public void deleteCustomer(UUID customerId) {
+        restTemplate.delete(apiHost + CUSTOMER_PATH_V1 + customerId);
     }
 }
--- SpringCloud/05.RestTemplate-mssc-brewery-client-master/src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ SpringCloud/05.RestTemplate-mssc-brewery-client-master/src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.net.URI;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BreweryClientTest {
@@ -21,6 +22,7 @@ class BreweryClientTest {
         BeerDto beer = breweryClient.getBeerById(UUID.randomUUID());
         assertNotNull(beer);
     }
+
     @Test
     void postBeer() {
         BeerDto goodBeer = BeerDto.builder()
@@ -36,11 +38,40 @@ class BreweryClientTest {
         BeerDto goodBeer = BeerDto.builder()
                 .beerName("GoodBeer")
                 .build();
-        breweryClient.updateBeer(UUID.randomUUID(),goodBeer);
+        breweryClient.updateBeer(UUID.randomUUID(), goodBeer);
     }
 
     @Test
     void deleteBeer() {
         breweryClient.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto beer = breweryClient.getCustomerById(UUID.randomUUID());
+        assertNotNull(beer);
+    }
+
+    @Test
+    void postCustomer() {
+        CustomerDto goodCustomer = CustomerDto.builder()
+                .name("GoodBeer")
+                .build();
+        URI uri = breweryClient.saveNewCustomer(goodCustomer);
+        assertNotNull(uri);
+        System.out.println(uri);
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerDto goodCustomer = CustomerDto.builder()
+                .name("GoodBeer")
+                .build();
+        breweryClient.updateCustomer(UUID.randomUUID(), goodCustomer);
+    }
+
+    @Test
+    void deleteCustomer() {
+        breweryClient.deleteCustomer(UUID.randomUUID());
     }
 }
