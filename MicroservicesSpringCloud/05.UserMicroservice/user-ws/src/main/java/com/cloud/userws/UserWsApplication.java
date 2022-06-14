9c64bebf683ea08a5f7b9151d4e01065593b81c5
andychen <strikecbu@gmail.com>
2022-06-15 00:28:18 +0800

    [MicroSrvSpringCloud] Implement User microservice and health check with server send event(SSE)

--- MicroservicesSpringCloud/05.UserMicroservice/user-ws/src/main/java/com/cloud/userws/UserWsApplication.java
+++ MicroservicesSpringCloud/05.UserMicroservice/user-ws/src/main/java/com/cloud/userws/UserWsApplication.java
@@ -1,13 +1,33 @@
 package com.cloud.userws;
 
+import com.cloud.userws.ui.controllers.UserController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserWsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserWsApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(UserController userController) {
+        return args -> {
+            Flux.interval(Duration.ofSeconds(1))
+                    .doOnNext(index -> {
+                        userController.getStatus().tryEmitNext("Check status at " + LocalDateTime.now()+ "\n");
+                    })
+                    .subscribe();
+        };
     }
 
 }
---
+++ MicroservicesSpringCloud/05.UserMicroservice/user-ws/src/main/java/com/cloud/userws/ui/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.cloud.userws.ui.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Sinks.Many<String> status = Sinks.many()
+            .replay()
+            .latest();
+
+    public Sinks.Many<String> getStatus() {
+        return status;
+    }
+
+    @GetMapping(value = "/check/status", produces = {MediaType.APPLICATION_NDJSON_VALUE})
+    public Flux<String> healthCheck() {
+        return status.asFlux();
+    }
+}
