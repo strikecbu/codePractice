e8aa4b0f2248cf38919044833fb61566d8055605
andychen <strikecbu@gmail.com>
2022-06-16 23:02:53 +0800

    [MicroSrvSpringCloud] Add new end-point to user service and change eureka instance id to random

--- MicroservicesSpringCloud/05.UserMicroservice/user-ws/src/main/java/com/cloud/userws/ui/controllers/UserController.java
+++ MicroservicesSpringCloud/05.UserMicroservice/user-ws/src/main/java/com/cloud/userws/ui/controllers/UserController.java
@@ -1,26 +1,38 @@
 package com.cloud.userws.ui.controllers;
 
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+
+    private final Environment environment;
     private final Sinks.Many<String> status = Sinks.many()
             .replay()
             .latest();
+
+    public UserController(Environment environment) {
+        this.environment = environment;
+    }
 
     public Sinks.Many<String> getStatus() {
         return status;
     }
 
-    @GetMapping(value = "/status/check", produces = {MediaType.APPLICATION_NDJSON_VALUE})
-    public Flux<String> healthCheck() {
+    @GetMapping(value = "/status/check/stream", produces = {MediaType.APPLICATION_NDJSON_VALUE})
+    public Flux<String> healthCheckStream() {
         return status.asFlux();
+    }
+    @GetMapping(value = "/status/check")
+    public Mono<String> healthCheck() {
+        return Mono.just("Working on port: " + environment.getProperty("local.server.port"));
     }
 }
