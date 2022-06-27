57aa054d745951f4a1f12e024239d17a9953369f
andychen <strikecbu@gmail.com>
2022-06-27 20:59:09 +0800

    [MasterMicroservices] Implement decrypt from config server and refresh endpoint of client

--- MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -2,15 +2,13 @@ package com.eazybytes.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 }
