0ccfac7e714cdd626a77664b1279873fbd776bb9
andychen <strikecbu@gmail.com>
2022-06-29 22:34:44 +0800

    [MasterMicroservices] Add eureka server

---
+++ MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/eureka-server/src/test/java/com/cloud/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
