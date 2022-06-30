da91c87ebb60c8b86a624972a873c79dd9c86add
andychen <strikecbu@gmail.com>
2022-06-30 13:58:38 +0800

    [MasterMicroservices] Implement eureka server containerize and fix loan service link postgres in container issue

--- MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/loans/src/main/java/com/eazybytes/loans/LoansApplication.java
+++ MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -1,15 +1,7 @@
 package com.eazybytes.loans;
 
-import io.r2dbc.spi.ConnectionFactories;
-import io.r2dbc.spi.ConnectionFactory;
-import io.r2dbc.spi.ConnectionFactoryOptions;
-import io.r2dbc.spi.Option;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-
-import static io.r2dbc.spi.ConnectionFactoryOptions.*;
 
 @SpringBootApplication
 public class LoansApplication {
--- MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/loans/src/main/java/com/eazybytes/loans/repository/RepositoryConfig.java
+++ MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/loans/src/main/java/com/eazybytes/loans/repository/RepositoryConfig.java
@@ -4,28 +4,49 @@ import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
 import io.r2dbc.spi.Option;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 import static io.r2dbc.spi.ConnectionFactoryOptions.*;
 import static io.r2dbc.spi.ConnectionFactoryOptions.DATABASE;
 
 @Configuration
 @EnableR2dbcAuditing
+@ConfigurationProperties(prefix = "db.postgres")
+@Data
 public class RepositoryConfig {
+
+    private String url;
+    private String database;
+    private String username;
+    private String password;
+
+
     @Bean
     ConnectionFactory connectionFactory() {
-//        ConnectionFactoryBuilder.withUrl()
-        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
-                .option(DRIVER, "postgresql")
-                .option(HOST, "localhost")
-                .option(PORT, 5432)  // optional, defaults to 5432
-                .option(USER, "postgres")
-                .option(PASSWORD, "p@ssw0rd")
-                .option(DATABASE, "postgres")  // optional
-                .option(Option.valueOf("schema"), "public")
-                .build());
+        return ConnectionFactoryBuilder
+                .withUrl(url)
+                .database(database)
+                .username(username)
+                .password(password)
+                .configure(option -> {
+                    option.option(Option.valueOf("schema"), "public");
+                })
+                .build();
+
+//        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
+//                .option(DRIVER, "postgresql")
+//                .option(HOST, "localhost")
+//                .option(PORT, 5432)  // optional, defaults to 5432
+//                .option(USER, "postgres")
+//                .option(PASSWORD, "p@ssw0rd")
+//                .option(DATABASE, "postgres")  // optional
+//                .option(Option.valueOf("schema"), "public")
+//                .build());
     }
 }
