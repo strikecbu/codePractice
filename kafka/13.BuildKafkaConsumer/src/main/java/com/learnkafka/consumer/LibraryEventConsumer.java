40b4d640e368d0e9c6977da075507be5d3263a06
andychen <strikecbu@gmail.com>
2022-07-05 00:48:41 +0800

    [Kafka] Implement simple consumer kafka message

---
+++ kafka/13.BuildKafkaConsumer/src/main/java/com/learnkafka/consumer/LibraryEventConsumer.java
@@ -0,0 +1,17 @@
+package com.learnkafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventConsumer {
+
+    @KafkaListener(topics = {"library-events"})
+    public void consumer(ConsumerRecord<Integer, String> record) {
+        log.info("consumer record: {}", record);
+    }
+
+}
