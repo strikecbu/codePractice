8c85a75b4841162ce5d1a26ad62ec9977c1f397c
andychen <strikecbu@gmail.com>
2022-07-04 23:46:43 +0800

    [Kafka] 覆寫特別config做紀錄

--- kafka/08.BuildKafkaProducer/src/main/java/com/learnkafka/config/KafkaConfig.java
+++ kafka/08.BuildKafkaProducer/src/main/java/com/learnkafka/config/KafkaConfig.java
@@ -26,6 +26,8 @@ public class KafkaConfig {
     public KafkaSender<Integer, String> kafkaSender() {
         Map<String, Object> producerProps = new HashMap<>();
         producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        producerProps.put(ProducerConfig.ACKS_CONFIG, "all"); // 1(default) and all or -1 保證送給leader或是全部的follower也都收到
+        producerProps.put(ProducerConfig.RETRIES_CONFIG, 10); // 重送次數
         producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
         producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         SenderOptions<Integer, String> senderOptions =
