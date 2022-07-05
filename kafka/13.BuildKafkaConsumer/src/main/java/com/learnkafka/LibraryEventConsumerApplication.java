04b4ff291417c364276bd8264115f21e1a2758e2
andychen <strikecbu@gmail.com>
2022-07-06 00:16:22 +0800

    [Kafka] Implement commit_offset manual way

--- kafka/13.BuildKafkaConsumer/src/main/java/com/learnkafka/LibraryEventConsumerApplication.java
+++ kafka/13.BuildKafkaConsumer/src/main/java/com/learnkafka/LibraryEventConsumerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-@EnableKafka
 public class LibraryEventConsumerApplication {
 
 	public static void main(String[] args) {
