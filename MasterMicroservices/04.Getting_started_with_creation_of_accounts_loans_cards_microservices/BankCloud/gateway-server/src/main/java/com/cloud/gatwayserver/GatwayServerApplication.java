8677072034c6089aef6fa3308607e752d19533a1
andychen <strikecbu@gmail.com>
2022-07-07 21:32:04 +0800

    [MasterMicroservices] Add gateway service resources

---
+++ MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/gateway-server/src/main/java/com/cloud/gatwayserver/GatwayServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.gatwayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatwayServerApplication.class, args);
+	}
+
+}
---
+++ MasterMicroservices/04.Getting_started_with_creation_of_accounts_loans_cards_microservices/BankCloud/gateway-server/src/test/java/com/cloud/gatwayserver/GatwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.gatwayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatwayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
