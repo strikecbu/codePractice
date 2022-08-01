4d4771684907372a3639836fbc19001b1560aee7
andychen <strikecbu@gmail.com>
2022-08-01 22:12:27 +0800

    [Parallelism] Add lecture resources

---
+++ MultithreadingParallel/12.CompletableFuture/src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
@@ -0,0 +1,16 @@
+package com.learnjava.completablefuture;
+
+import com.learnjava.service.HelloWorldService;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.learnjava.util.LoggerUtil.log;
+
+
+public class CompletableFutureHelloWorld {
+
+    public static void main(String[] args) {
+
+
+    }
+}
