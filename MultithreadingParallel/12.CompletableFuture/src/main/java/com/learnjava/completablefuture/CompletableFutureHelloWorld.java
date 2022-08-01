23f77d147eec9c7d0f2c1dd888c804a72731124f
andychen <strikecbu@gmail.com>
2022-08-01 22:18:54 +0800

    [Parallelism] Implement first CompletableFuture

--- MultithreadingParallel/12.CompletableFuture/src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
+++ MultithreadingParallel/12.CompletableFuture/src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
@@ -4,13 +4,22 @@ import com.learnjava.service.HelloWorldService;
 
 import java.util.concurrent.CompletableFuture;
 
+import static com.learnjava.util.CommonUtil.delay;
 import static com.learnjava.util.LoggerUtil.log;
 
 
 public class CompletableFutureHelloWorld {
 
     public static void main(String[] args) {
+        HelloWorldService hws = new HelloWorldService();
+        CompletableFuture.supplyAsync(hws::helloWorld)
+                .thenAccept(result -> {
+                    log("Result is " + result);
+                })
+                .join();
 
+        log("Done!");
+//        delay(2000);
 
     }
 }
