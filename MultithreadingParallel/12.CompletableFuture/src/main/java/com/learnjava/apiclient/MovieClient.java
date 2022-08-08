a4753ecab99bfa7cb3241e620fa12c0aa0747eed
andychen <strikecbu@gmail.com>
2022-08-09 00:14:04 +0800

    [Parallelism] Implement create webClient to get movie data using normal blocking way and completableFuture way

---
+++ MultithreadingParallel/12.CompletableFuture/src/main/java/com/learnjava/apiclient/MovieClient.java
@@ -0,0 +1,68 @@
+package com.learnjava.apiclient;
+
+import com.learnjava.domain.movie.Movie;
+import com.learnjava.domain.movie.MovieInfo;
+import com.learnjava.domain.movie.Review;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class MovieClient {
+
+    private final WebClient webClient;
+
+    public MovieClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    private MovieInfo retrieveMovieInfo(Long movieInfoId) {
+        return webClient
+                .get()
+                .uri("/v1/movie_infos/{movieInfoId}", movieInfoId)
+                .retrieve()
+                .bodyToMono(MovieInfo.class)
+                .block();
+    }
+
+    private List<Review> retrieveReview(Long movieInfoId) {
+        String uriString = UriComponentsBuilder.fromUriString("/v1/reviews")
+                .queryParam("movieInfoId", movieInfoId)
+                .buildAndExpand()
+                .toUriString();
+
+        return webClient
+                .get()
+                .uri(uriString)
+                .retrieve()
+                .bodyToFlux(Review.class)
+                .collectList()
+                .block();
+    }
+
+    public Movie retrieveMovie(Long movieInfoId) {
+        MovieInfo movieInfo = retrieveMovieInfo(movieInfoId);
+        List<Review> reviews = retrieveReview(movieInfoId);
+        return new Movie(movieInfo, reviews);
+    }
+
+    public CompletableFuture<Movie> retrieveMovie_CF(Long movieInfoId) {
+        var movieInfo = CompletableFuture.supplyAsync(() -> retrieveMovieInfo(movieInfoId));
+        var reviews = CompletableFuture.supplyAsync(() -> retrieveReview(movieInfoId));
+        return movieInfo.thenCombine(reviews, Movie::new);
+    }
+
+    public List<Movie> retrieveMovies(List<Long> movieInfoIds) {
+        return movieInfoIds.stream()
+                .map(this::retrieveMovie)
+                .collect(Collectors.toList());
+    }
+    public List<Movie> retrieveMovies_CF(List<Long> movieInfoIds) {
+        return movieInfoIds.stream()
+                .map(this::retrieveMovie_CF)
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+    }
+}
---
+++ MultithreadingParallel/12.CompletableFuture/src/test/java/com/learnjava/apiclient/MovieClientTest.java
@@ -0,0 +1,68 @@
+package com.learnjava.apiclient;
+
+import com.learnjava.domain.movie.Movie;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieClientTest {
+
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("http://localhost:8080/movies")
+            .build();
+
+    private final MovieClient movieClient = new MovieClient(webClient);
+
+    @RepeatedTest(10)
+    void retrieveMovie() {
+        startTimer();
+        Long movieInfoId = 1L;
+
+        Movie movie = movieClient.retrieveMovie(movieInfoId);
+
+        timeTaken();
+        assert movie != null;
+        assertEquals("Batman Begins", movie.getMovieInfo().getName());
+        assertEquals(1, movie.getReviewList().size());
+    }
+
+    @RepeatedTest(10)
+    void retrieveMovie_CF() {
+        startTimer();
+        Long movieInfoId = 1L;
+
+        Movie movie = movieClient.retrieveMovie_CF(movieInfoId).join();
+
+        timeTaken();
+        assert movie != null;
+        assertEquals("Batman Begins", movie.getMovieInfo().getName());
+        assertEquals(1, movie.getReviewList().size());
+    }
+
+    @RepeatedTest(10)
+    void retrieveMovies() {
+        startTimer();
+        List<Long> movieInfoIds = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L );
+
+        var movies = movieClient.retrieveMovies(movieInfoIds);
+
+        timeTaken();
+        assertEquals(7, movies.size());
+    }
+    @RepeatedTest(10)
+    void retrieveMovies_CF() {
+        startTimer();
+        List<Long> movieInfoIds = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L );
+
+        var movies = movieClient.retrieveMovies_CF(movieInfoIds);
+
+        timeTaken();
+        assertEquals(7, movies.size());
+    }
+}
