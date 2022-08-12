a2a57675b810eab97fbb28197d553d20277446bf
andychen <strikecbu@gmail.com>
2022-08-12 23:28:36 +0800

    [Kafka] Add custom default error handling

--- kafka/13.BuildKafkaConsumer/src/main/java/com/learnkafka/config/KafkaConsumerConfig.java
+++ kafka/13.BuildKafkaConsumer/src/main/java/com/learnkafka/config/KafkaConsumerConfig.java
@@ -1,8 +1,18 @@
 package com.learnkafka.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @EnableKafka
@@ -14,16 +24,22 @@ public class KafkaConsumerConfig {
         this.properties = properties;
     }
 
-//    @Bean
-//    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
-//            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
-//            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
-//        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-//        configurer.configure(factory,
-//                kafkaConsumerFactory.getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.properties.buildConsumerProperties())));
-//        factory.getContainerProperties()
-//                .setAckMode(ContainerProperties.AckMode.MANUAL);
-//        factory.setConcurrency(3);
-//        return factory;
-//    }
+    private DefaultErrorHandler commonErrorHandler() {
+        FixedBackOff backOff = new FixedBackOff(1000L, 2);
+        return new DefaultErrorHandler(backOff);
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory,
+                kafkaConsumerFactory.getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.properties.buildConsumerProperties())));
+        factory.getContainerProperties()
+                .setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setConcurrency(3);
+        factory.setCommonErrorHandler(commonErrorHandler());
+        return factory;
+    }
 }
