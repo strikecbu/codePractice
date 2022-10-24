a38937c871eb7a7321471b69218b618a03326257
andychen <strikecbu@gmail.com>
2022-10-24 22:50:38 +0800

    [NextJS] Add meetup detail page

---
+++ Nextjs/14.Summary/lecture-practice/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetailPage() {
+  return (
+    <MeetupDetail
+      image="https://rainieis.tw/wp-content/uploads/IMG_3355.jpg"
+      title="House"
+      address="Some street No5. Some city"
+      description="This is a good place"
+    ></MeetupDetail>
+  );
+}
+
+export default MeetupDetailPage;
