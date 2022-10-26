ca479753e3281273a4b85337184ec27734f48f97
andychen <strikecbu@gmail.com>
2022-10-26 23:33:24 +0800

    [SpringSecurity] Implement use InMemoryUserDetailsManager to set users

--- SpringSecurity/03.managerUsers/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ SpringSecurity/03.managerUsers/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -3,40 +3,58 @@ package com.eazybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        /**
-         *  Below is the custom security configurations
-         */
-
         http.authorizeHttpRequests()
                         .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
                         .requestMatchers("/notices","/contact").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
-
-        /**
-         *  Configuration to deny all the requests
-         */
-        /*http.authorizeHttpRequests().anyRequest().denyAll()
-                .and().formLogin()
-                .and().httpBasic();
-        return http.build();*/
-
-        /**
-         *  Configuration to permit all the requests
-         */
-        /*http.authorizeHttpRequests().anyRequest().permitAll()
-                .and().formLogin()
-                .and().httpBasic();
-        return http.build();*/
     }
+
+    @Bean
+    public InMemoryUserDetailsManager manageUser() {
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("user")
+                .build();
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("12345")
+//                .authorities("user")
+//                .build();
+
+
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
 
 }
