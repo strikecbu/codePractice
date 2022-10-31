04a62bd1574e31ed59f69fdeb50d348c41c61296
andychen <strikecbu@gmail.com>
2022-11-01 00:25:16 +0800

    [SpringSecurity] Implement to use customize user detail service to manage user details

--- SpringSecurity/03.managerUsers/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ SpringSecurity/03.managerUsers/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -3,12 +3,13 @@ package com.eazybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -17,44 +18,52 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                        .requestMatchers("/notices","/contact").permitAll()
-                .and().formLogin()
-                .and().httpBasic();
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
+                .authenticated()
+                .requestMatchers("/notices", "/contact")
+                .permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager manageUser() {
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("user")
-                .build();
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
+//    @Bean
+//    public InMemoryUserDetailsManager manageUser() {
+//        UserDetails admin = User.withUsername("admin")
 //                .password("12345")
 //                .authorities("admin")
 //                .build();
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
+//        UserDetails user = User.withUsername("user")
 //                .password("12345")
 //                .authorities("user")
 //                .build();
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("12345")
+////                .authorities("admin")
+////                .build();
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("12345")
+////                .authorities("user")
+////                .build();
+//
+//
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
-
-
-        return new InMemoryUserDetailsManager(admin, user);
-    }
+//    @Bean
+//    public UserDetailsService jdbcManager(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
-
 
 
 }
