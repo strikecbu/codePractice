f90f41f0bf1658f3bd0d227549b161638baa6dfd
andychen <strikecbu@gmail.com>
2022-11-03 00:06:35 +0800

    [SpringSecurity] Change to use BCryptPasswordEncoder as password encoder.

--- SpringSecurity/03.managerUsers/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ SpringSecurity/03.managerUsers/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -3,13 +3,9 @@ package com.eazybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -17,7 +13,8 @@ public class ProjectSecurityConfig {
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf()
+                .disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
                 .authenticated()
@@ -63,7 +60,7 @@ public class ProjectSecurityConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 
--- SpringSecurity/03.managerUsers/src/main/java/com/eazybytes/controller/CustomerController.java
+++ SpringSecurity/03.managerUsers/src/main/java/com/eazybytes/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.eazybytes.model.Customer;
 import com.eazybytes.repository.CustomerRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerController {
 
+    private final PasswordEncoder passwordEncoder;
+
     private final CustomerRepository customerRepository;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    public CustomerController(PasswordEncoder passwordEncoder, CustomerRepository customerRepository) {
+        this.passwordEncoder = passwordEncoder;
         this.customerRepository = customerRepository;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody Customer customer) {
+        String hashPassword = passwordEncoder.encode(customer.getPwd());
+        customer.setPwd(hashPassword);
         Customer save = customerRepository.save(customer);
         if (save.getId() > 0) {
             return ResponseEntity.status(HttpStatus.CREATED).body("Register new customer success.");
