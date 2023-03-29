a31de37867d438e8b98141ce629a6d6811d1b436
andychen <strikecbu@gmail.com>
2023-03-29 23:03:54 +0800

    [SpringSecurity] [CORS&CSRF] Implement CORS settings for FrontEnd

--- SpringSecurity/06.CORs_CSRF/bank-app/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ SpringSecurity/06.CORs_CSRF/bank-app/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -6,18 +6,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                        .authorizeHttpRequests()
-                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                        .requestMatchers("/notices","/contact","/register").permitAll()
-                .and().formLogin()
-                .and().httpBasic();
+        http.cors()
+                .configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowCredentials(true);
+                    corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                    corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                    corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    corsConfiguration.setMaxAge(3600L);
+                    return corsConfiguration;
+                })
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
+                .authenticated()
+                .requestMatchers("/notices", "/contact", "/register")
+                .permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
         return http.build();
     }
 
