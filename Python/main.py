235d16fdb0a9ce84bf017c1e317d9274b0353d81
andychen <strikecbu@gmail.com>
2023-05-02 00:49:35 +0800

    [Python] Add validate

--- Python/main.py
+++ Python/main.py
@@ -9,7 +9,22 @@ def convert_days_to_unit(number_of_days):
     return f"{number_of_days} days are {number_of_days * numbers_of_unit} {name_of_unit}"
 
 
+def validate_and_execute():
+    try:
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = convert_days_to_unit(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered a 0, please enter a valid positive number")
+        else:
+            print("You entered a negative number, no conversion for you")
+    except ValueError:
+        print("Your input is not a number. Don't ruin my program")
+
+
 # 3. Print the result
-numbers_of_days = input("Enter the number of days you want to convert to hours: \n")
-convert_result = convert_days_to_unit(int(numbers_of_days))
-print(convert_result)
+user_input = ""
+while user_input != "exit":
+    user_input = input("Enter the number of days you want to convert to hours: \n")
+    validate_and_execute()
