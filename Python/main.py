7225a87be92f2820853828888702f54f2c9e1242
andychen <strikecbu@gmail.com>
2023-05-02 01:00:20 +0800

    [Python] user can input unit they want

--- Python/main.py
+++ Python/main.py
@@ -1,19 +1,20 @@
 # create a calculator that can convert user input days to hours
-# 1. Create the variables for the number of days and hours per day
-numbers_of_unit = 24
-name_of_unit = "hours"
+# 2. Calculate the number of hours by multiplying the number of days by user input unit
+def convert_days_to_unit(number_of_days, name_of_unit):
+    if name_of_unit == "hours":
+        return f"{number_of_days} days are {number_of_days * 24} hours"
+    elif name_of_unit == "minutes":
+        return f"{number_of_days} days are {number_of_days * 24 * 60} minutes"
+    else:
+        return "unsupported unit"
 
 
-# 2. Calculate the number of hours by multiplying the number of days by 24
-def convert_days_to_unit(number_of_days):
-    return f"{number_of_days} days are {number_of_days * numbers_of_unit} {name_of_unit}"
-
-
-def validate_and_execute():
+def validate_and_execute(user_input_split):
     try:
-        user_input_number = int(user_input)
+        user_input_dictionary = {"days": user_input_split[0], "unit": user_input_split[1]}
+        user_input_number = int(user_input_dictionary["days"])
         if user_input_number > 0:
-            calculated_value = convert_days_to_unit(user_input_number)
+            calculated_value = convert_days_to_unit(user_input_number, user_input_dictionary["unit"])
             print(calculated_value)
         elif user_input_number == 0:
             print("You entered a 0, please enter a valid positive number")
@@ -21,10 +22,13 @@ def validate_and_execute():
             print("You entered a negative number, no conversion for you")
     except ValueError:
         print("Your input is not a number. Don't ruin my program")
+    except IndexError:
+        print("Your input is not match format. Don't ruin my program")
 
 
 # 3. Print the result
 user_input = ""
 while user_input != "exit":
-    user_input = input("Enter the number of days you want to convert to hours: \n")
-    validate_and_execute()
+    user_input = input("Enter the number of days and unit you want to convert, split with colon\n")
+    user_split_data = user_input.split(":")
+    validate_and_execute(user_split_data)
